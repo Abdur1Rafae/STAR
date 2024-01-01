6905f60cd6b96bf13683749f2d78b1790e058bc0
Saifullah Khan <saif4rmkhan@gmail.com>
2024-01-01 20:06:00 +0500

    account mananger page done

---
+++ star/src/components/SubmitButton.jsx
@@ -0,0 +1,23 @@
+// SubmitButton.js
+import React from 'react';
+
+const SubmitButton = ({ onClick, children }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded focus:outline-none focus:shadow-outline"
+      style={{
+        width: '180px',
+        height: '45px',
+        borderRadius: '8px',
+        background: '#274C77',
+        position: 'relative',
+        // Add other styles as needed
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default SubmitButton;
