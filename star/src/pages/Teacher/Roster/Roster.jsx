6ac76143a3e317c76b5fdb18107a39031ca2109d
hasaansaeed02 <hasaansaeed02@gmail.com>
2024-02-23 16:10:11 +0500

    Roster and Add Students
    First edition

---
+++ star/src/pages/Teacher/Roster/Roster.jsx
@@ -0,0 +1,180 @@
+import React from "react";
+import { HoverState } from "./HoverState";
+import { IconClose } from "./IconClose";
+import { IconPlus } from "./IconPlus";
+import { ToolbarUrlControls } from "./ToolbarUrlControls";
+import "./style.css";
+
+export const ClassRoster = () => {
+  return (
+    <div className="class-roster">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <img className="rectangle" alt="Rectangle" src="rectangle-51.svg" />
+          <img className="rectangle-2" alt="Rectangle" src="rectangle-49.svg" />
+          <div className="browser-chrome-dark">
+            <div className="browser-chrome-light">
+              <div className="overlap-group">
+                <div className="browser-URL-controls">
+                  <ToolbarUrlControls
+                    URLBarClassName="toolbar-URL-controls-3"
+                    className="toolbar-URL-controls-instance"
+                    divClassName="toolbar-URL-controls-4"
+                    divClassNameOverride="toolbar-URL-controls-5"
+                    forwardColor="#86888A"
+                    hasImageUserProfile={false}
+                    iconArrowFavoriteContainer="container-8.svg"
+                    iconArrowHomeContainer="container-10.svg"
+                    iconArrowMoreContainer="container-11.svg"
+                    iconArrowMoreIconArrowMoreClassName="toolbar-URL-controls-2"
+                    iconArrowSecureContainer="container-9.svg"
+                    iconBackColor="white"
+                    iconRefreshColor="white"
+                    text="mindLoom.com"
+                    text1="/Assessment_Scheduling_Screen"
+                    toolbarUrlControlsClassName="design-component-instance-node"
+                  />
+                  <div className="toolbar-browser">
+                    <div className="broswer-control-bar">
+                      <div className="tab-plus">
+                        <div className="browser-tab-with">
+                          <div className="frame-2">
+                            <div className="frame-3">
+                              <img className="img-2" alt="Curve l" src="curve-l.svg" />
+                              <div className="favicon-text-icons">
+                                <p className="tab-name">
+                                  <span className="span">mindLoom&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+                                  <span className="text-wrapper-2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+                                  <span className="text-wrapper-3">.</span>
+                                </p>
+                                <IconClose className="icon-close" color="white" />
+                              </div>
+                              <img className="img-2" alt="Curve r" src="curve-r.svg" />
+                            </div>
+                            <IconPlus className="icon-plus" color="#BDC1C6" />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="browser-controls">
+                        <div className="option-expand" />
+                        <div className="option-minimize" />
+                        <div className="option-close" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="browser-outline" />
+              </div>
+            </div>
+          </div>
+          <div className="group">
+            <div className="text-wrapper-4">Muhammad Maaz Arsalan Batla</div>
+            <div className="text-wrapper-5">22794</div>
+            <div className="text-wrapper-6">m.batla.22792@khi.iba.edu.pk</div>
+          </div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-2">
+              <div className="group-wrapper">
+                <div className="div-wrapper">
+                  <div className="group-2">
+                    <div className="div-wrapper">
+                      <div className="group-2">
+                        <div className="group-3">
+                          <img className="rectangle-3" alt="Rectangle" src="rectangle-4.png" />
+                          <img className="rectangle-4" alt="Rectangle" src="rectangle-5.png" />
+                          <img className="rectangle-5" alt="Rectangle" src="rectangle-2.png" />
+                          <img className="rectangle-6" alt="Rectangle" src="rectangle-3.png" />
+                          <img className="rectangle-7" alt="Rectangle" src="image.png" />
+                          <img className="rectangle-8" alt="Rectangle" src="rectangle-3-2.png" />
+                          <img className="rectangle-9" alt="Rectangle" src="rectangle-3-3.png" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="group-4" />
+            </div>
+          </div>
+          <p className="p">5046 (11:30 AM - 12:45 PM )</p>
+          <img className="pop-up-screen" alt="Pop up screen" src="pop-up-screen.png" />
+          <img className="rectangle-10" alt="Rectangle" src="rectangle-43.png" />
+          <img className="rectangle-11" alt="Rectangle" src="rectangle-53.png" />
+          <img className="rectangle-12" alt="Rectangle" src="rectangle-54.png" />
+          <img className="rectangle-13" alt="Rectangle" src="rectangle-55.png" />
+          <img className="rectangle-14" alt="Rectangle" src="rectangle-56.png" />
+          <img className="rectangle-15" alt="Rectangle" src="rectangle-57.png" />
+          <img className="rectangle-16" alt="Rectangle" src="rectangle-58.png" />
+          <img className="rectangle-17" alt="Rectangle" src="rectangle-59.png" />
+          <img className="rectangle-18" alt="Rectangle" src="rectangle-60.png" />
+          <img className="rectangle-19" alt="Rectangle" src="rectangle-61.png" />
+          <img className="rectangle-20" alt="Rectangle" src="rectangle-62.png" />
+          <img className="line" alt="Line" src="line-4.svg" />
+          <div className="group-5">
+            <div className="text-wrapper-7">Name</div>
+            <div className="text-wrapper-8">Student ID</div>
+            <div className="text-wrapper-9">Email</div>
+          </div>
+          <div className="group-6">
+            <div className="text-wrapper-10">Maaz Shamim</div>
+            <div className="text-wrapper-11">22792</div>
+            <div className="text-wrapper-12">m.shamim.22792@khi.iba.edu.pk</div>
+          </div>
+          <div className="group-7">
+            <div className="text-wrapper-10">Saifullah Khan</div>
+            <div className="text-wrapper-11">22878</div>
+            <div className="text-wrapper-12">m.saif.22792@khi.iba.edu.pk</div>
+          </div>
+          <div className="group-8">
+            <div className="group-9">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-13">Delete Room</div>
+                <img className="plus" alt="Plus" src="plus-3.png" />
+              </div>
+            </div>
+          </div>
+          <div className="group-10">
+            <div className="group-11">
+              <div className="overlap-group-3">
+                <div className="text-wrapper-14">Add Students</div>
+              </div>
+            </div>
+          </div>
+          <div className="group-12">
+            <div className="text-wrapper-15">Muhammad Abdur Rafae</div>
+            <div className="overlap-3">
+              <div className="text-wrapper-16">22828</div>
+              <div className="text-wrapper-17">m.rafae.22792@khi.iba.edu.pk</div>
+              <HoverState className="hover-state-instance" />
+            </div>
+          </div>
+          <div className="text-wrapper-18">Introduction to Computing</div>
+          <div className="group-13">
+            <div className="overlap-4">
+              <img className="rectangle-21" alt="Rectangle" src="rectangle-18.png" />
+              <img className="rectangle-22" alt="Rectangle" src="rectangle-19.png" />
+            </div>
+          </div>
+          <div className="group-14">
+            <div className="overlap-4">
+              <img className="rectangle-21" alt="Rectangle" src="rectangle-18-2.png" />
+              <img className="rectangle-22" alt="Rectangle" src="rectangle-19-2.png" />
+            </div>
+          </div>
+          <div className="group-15">
+            <div className="text-wrapper-19">Hasaan Ahmed Saeed</div>
+            <div className="text-wrapper-11">22123</div>
+            <div className="text-wrapper-12">m.hasaan.22792@khi.iba.edu.pk</div>
+          </div>
+          <img className="rectangle-23" alt="Rectangle" src="rectangle-48.svg" />
+          <img className="rectangle-24" alt="Rectangle" src="rectangle-50.png" />
+          <img className="rectangle-25" alt="Rectangle" src="rectangle-52.png" />
+          <img className="plus-2" alt="Plus" src="plus.png" />
+          <img className="plus-3" alt="Plus" src="plus-2.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Classes
