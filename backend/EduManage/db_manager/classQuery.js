54614bfd488d5edd95ba4a93651c478417233e41
Muhammad Abdur Rafae <114411197+Abdur1Rafae@users.noreply.github.com>
2024-03-24 17:10:41 +0500

    Integrate Classes page with backend
    fix class query
    add adding class dialog

--- backend/EduManage/db_manager/classQuery.js
+++ backend/EduManage/db_manager/classQuery.js
@@ -9,7 +9,7 @@ const QUERY =
     ADD_STUDENTS : 'Insert IGNORE into user (Email, FirstName, LastName, Role, ERP) VALUES ?; Insert IGNORE into Enrollment (SectionID, StudentID) VALUES ?;',
     REMOVE_STUDENT : 'Delete from Enrollment where StudentID = ? AND SectionID = ?',
     GET_ROSTER : 'SELECT CONCAT(u.FirstName, " ", u.LastName) AS Name, u.Email, u.ERP FROM User u JOIN Enrollment e ON e.StudentID = u.Email WHERE e.SectionID = ?',
-    GET_CLASSES : 'Select ClassID, ClassName, JSON_ARRAYAGG(JSON_OBJECT("SectionID", SectionID, "SectionName", SectionName)) AS Sections from Class JOIN Section using(ClassID) where TeacherID = ? GROUP BY ClassName, ClassID;',
+    GET_CLASSES : 'Select ClassID, ClassName, JSON_ARRAYAGG(JSON_OBJECT("SectionID", SectionID, "SectionName", SectionName)) AS Sections from Class LEFT JOIN Section using(ClassID) where TeacherID = ? GROUP BY ClassName, ClassID;',
 
     GET_ENROLLED_CLASSES : 'SELECT SectionID, ClassName, Count(AssessmentID) As Assessments FROM Arete.Enrollment JOIN Assessments using (SectionID) where StudentID = ? GROUP BY ClassName, SectionID;'
 }
---
+++ star/src/APIS/Teacher/ClassAPI.js
@@ -0,0 +1,59 @@
+import { AxiosBase } from '../BaseUrl';
+
+const token = process.env.REACT_APP_ACCESS_TOKEN
+
+const GetAllClasses = async () => {
+    console.log(process.env)
+    const res = await AxiosBase.get(`edumanage/class/my-classes`,{
+        headers: {
+            authorization: `Bearer ${token}`
+        }
+    })
+
+    return res.data;
+};
+
+const AddClass = async({name}) => {
+    const res = await AxiosBase.post('edumanage/class/new-class',{
+        className: name
+    },{
+        headers: {
+            authorization: `Bearer ${token}`
+        }
+    })
+
+    return res.data
+}
+
+const DeleteClass = async ({ id }) => {
+    try {
+        const res = await AxiosBase.delete(`edumanage/class/delete-class`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            data: {
+                classId: id
+            }
+        });        
+
+        return res.data;
+    } catch (error) {
+        console.error('Error deleting class:', error);
+    }
+};
+
+const UpdateClass = async({id, name}) => {
+    const res = await AxiosBase.put('edumanage/class/update-class',{
+        className: name,
+        classId: id
+    },{
+        headers: {
+            authorization: `Bearer ${token}`
+        }
+    })
+
+    return res.data
+}
+
+
+export {GetAllClasses, AddClass, DeleteClass, UpdateClass}
