0d09ace804e186d252251b836b7cbc337c2f8475
Saifullah Khan <saif4rmkhan@gmail.com>
2024-03-25 17:09:11 +0500

    addquestion functionality done

--- star/src/pages/Teacher/AddQuestions.jsx
+++ star/src/pages/Teacher/AddQuestions.jsx
@@ -2,20 +2,25 @@ import React ,{ useState }from 'react';
 import MenuBar from '../../components/MenuBar'
 import SideBar from '../../components/Teacher/SideBar'
 import Subheader from '../../components/Teacher/Subheader'
-import { MdOutlineSettingsBackupRestore } from 'react-icons/md';
+import { MdOutlineSettingsBackupRestore ,MdClose } from 'react-icons/md';
 import { DoughnutGraph } from '../../components/Teacher/DoughnutGraph';
 import questionMCQ from '../../components/MCQ.png'
 import TFQuestion from '../../components/TF.png'
 import SAQuestion from '../../components/shortAnswer.png'
 import StoredQuestion from '../../components/Teacher/StoredQuestion';
-
+import QuestionCreator from '../../components/Teacher/QuestionCreator';
+import SkillFilter from '../../components/Teacher/SkillFilter';
+import DisplayOnlyQuestions from '../../components/Teacher/DisplayOnlyQuestions';
 
 
 function AddQuestions() {
     const [topics, setTopics] = useState([{name: "Differentiation", value: 8}, {name: "Integration", value: 5}, {name: "History of Computers", value: 12}])
     const skills = ["Problem Solving", "Logic Design", "Quantitative Analysis", "Critical Thinking"]
+    const difficulty = ['Easy', 'Medium', 'Hard'];
     const [creatingQuestion, setCreateQuestion] = useState(null);
     const [reuseDialog, setReuseDialog] = useState(false);
+    const [selectedSkill, setSelectedSkill] = useState("Logic")
+    const [selectedCategory, setSelectedCategory] = useState('');
     const [questions, setQuestions] = useState(
         [{
             type: "MCQ",
@@ -105,6 +110,34 @@ function AddQuestions() {
             updatedQuestions[index].point = point;
             setQuestions(updatedQuestions);
         };
+        const deleteQuestion = (id) => {
+            const updatedQuestions = questions.filter((_, index) => index !== id);
+            setQuestions(updatedQuestions);
+        };
+            
+        const handleCreateQuestion = (type) => {
+            setCreateQuestion(type); // Set the type of question to create
+        };
+        const handleCloseQuestionCreator = () => {
+            setCreateQuestion(null); // Close the QuestionCreator
+        };
+        
+        const handleReuseDialog = () => {
+            setReuseDialog(true); // Open the reuse dialog
+        };
+
+        const handleCloseReuseDialog = () => {
+        setReuseDialog(false); // Close the reuse dialog
+        };
+        const deleteTopicHandler = ({ index }) => {
+        const updatedTopics = [...topics];
+        updatedTopics.splice(index, 1);
+        setTopics(updatedTopics); 
+        };
+        const handleSelectCategory = (category) => {
+            setSelectedCategory(category);
+        }
+
 
   return (
     <div className=' w-full h-full font-body  border border-black '>
@@ -117,7 +150,7 @@ function AddQuestions() {
                     <div className='w-full flex flex-col items-center gap-4'>
                         <div className='w-full flex items-start justify-center gap-4'>
                             <div className='border-2 border-dotted border-slate-400'>
-                            <button className='w-24 h-24 flex flex-col items-center justify-center gap-1 border-2 border-white hover:border-DarkBlue hover:bg-LightBlue transition-colors duration-300' onClick={()=>setCreateQuestion("Multiple Choice Question")}>
+                            <button className='w-24 h-24 flex flex-col items-center justify-center gap-1 border-2 border-white hover:border-DarkBlue hover:bg-LightBlue transition-colors duration-300' onClick={()=> setCreateQuestion("Multiple Choice Question")}>
                                 <img className='w-12 mix-blend-multiply' src={questionMCQ} alt=''/>
                                 <p className='text-xs'>MCQ</p>
                             </button>
@@ -141,10 +174,62 @@ function AddQuestions() {
                             <p className='text-xs'>Reuse Question(s) from Question Banks</p>
                         </button>
                         </div>
+                         {/* Render TopicContainer based on reuseDialog state */}
+                         {reuseDialog &&
+                <div className='fixed top-0 left-0 w-full h-full bg-gray-700 bg-opacity-20 z-10'>
+                    <div className='relative inset-x-0 mx-auto top-20 w-11/12 md:w-7/12 h-5/6 overflow-y-auto bg-LightBlue z-10'>
+                        <div className='sticky top-0 bg-DarkBlue h-12 w-full flex text-white justify-between'>
+                            <h3 className='my-auto ml-2'>Select Questions to add</h3>
+                            <button className='mr-2' onClick={()=>setReuseDialog(false)}><MdClose className='text-lg'/></button>
+                        </div>
+                        <div className='p-4 flex flex-col gap-4'>
+                            <div className='flex gap-4 mb-4'>
+                                <div className='flex flex-col md:flex-row items-center'>
+                                    <p className='text-xs'>Skill Targeted :&nbsp; </p>
+                                    <SkillFilter selectedSkill={selectedSkill} setSelectSkill={setSelectedSkill}/>
+                                </div>
+                                <div className='flex flex-col md:flex-row items-center'>
+                                    <p className='text-xs'>Difficulty :&nbsp;</p>
+                                    <div className="md:h-6 text-xs h-6 bg-LightBlue border border-black rounded-md hover:border-gray-400 ">
+                                        <select
+                                            value={selectedCategory}
+                                            onChange={(e) => handleSelectCategory(e.target.value)}
+                                            className='outline-none bg-LightBlue rounded-md h-5'
+                                        >
+                                            {difficulty.map((category, index) => (
+                                                <option key={index} value={category}>
+                                                    {category}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </div>  
+                                </div>
+                            </div>
+                            {questions.map((question, index)=> {
+                                return <DisplayOnlyQuestions skill={question.skill} difficulty={question.difficulty} point={question.point} question={question.question} explanation={question.explanation} options={question.options} image={question.imageUrl}/>
+                            })}
+                            {questions.map((question, index)=> {
+                                return <DisplayOnlyQuestions skill={question.skill} difficulty={question.difficulty} point={question.point} question={question.question} explanation={question.explanation} options={question.options} image={question.imageUrl}/>
+                            })}
+                        </div>
+                        <div className='sticky border-t-2 border-black left-0 bottom-0 w-full h-12 bg-LightBlue flex justify-center items-center text-white'>
+                            <button className='bg-DarkBlue rounded-md px-2 py-1 min-w-16'>Save ({8})</button>
+                        </div>
+                    </div>
+                </div>
+            }
+
+                {creatingQuestion && ( // Render QuestionCreator if creatingQuestion is not null
+                        <QuestionCreator
+                            type={creatingQuestion}
+                            closeHandler={handleCloseQuestionCreator}
+                            savingHandler={updateQuestion}
+                        />
+                    )}
                         <div className='w-full flex flex-col gap-2'>
                         {
                             questions.map((question, index)=> {
-                                return <StoredQuestion savingHandler={updateQuestion} id={index} type={question.type} skill={question.skill} difficulty={question.difficulty} point={question.point} question={question.question} explanation={question.explanation} options={question.options} image={question.imageUrl}/>
+                                return <StoredQuestion deleteHandler={() => deleteQuestion(index)} savingHandler={updateQuestion} id={index} type={question.type} skill={question.skill} difficulty={question.difficulty} point={question.point} question={question.question} explanation={question.explanation} options={question.options} image={question.imageUrl}/>
                             })
                         }
                         </div>
