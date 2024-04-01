d571978528076741dc55d56ea61f446202394588
Maaz Shamim <m.shamim.22792@khi.iba.edu.pk>
2024-04-01 18:19:14 +0500

    Models Optimized
    -Mongoose models optimized to improve query performance
    -implemented a new microservice AssessHub that will be responsible for conducting assessments on the platform.

--- backend/TeacherHub/routes/assessmentManagementRoutes.js
+++ backend/TeacherHub/routes/assessmentManagementRoutes.js
@@ -6,7 +6,6 @@ router.post('/new-assessment', controller.createAssessment)
 router.delete('/delete-assessment/:assessmentId', controller.deleteAssessment)
 router.put('/update-assessment/:assessmentId', controller.updateAssessment)
 router.get('/scheduled-assessments', controller.getScheduledAssessments)
-router.get('/published-assessments', controller.getPublishedAssessments)
 router.get('/assessment-details/:assessmentId', controller.getAssessmentDetails)
 
 module.exports = router
