3aa947c71435e619b650c411ec472a457f5c1fb8
Maaz Shamim <m.shamim.22792@khi.iba.edu.pk>
2024-05-12 01:24:50 +0500

    UserGuardian Compete

--- backend/APIGateway/controllers/sessionController.js
+++ backend/APIGateway/controllers/sessionController.js
@@ -14,7 +14,6 @@ function createAccessToken(user, sessionId)
 {
     return jwt.sign({id: user._id, role:user.role, sessionId}, process.env.JWT_SECRET, {expiresIn: ACCESS_TOKEN_EXPIRATION})   
 }
-
 async function createSession(id)
 {
     const sessionId = uuid.v4() 
@@ -22,7 +21,6 @@ async function createSession(id)
     await client.hSet(SESSION_HASH_KEY, id, refreshToken)
     return sessionId
 }
-
 async function authenticateUser(user) 
 {
     const url = await getServiceURL(AUTHENTICATION_SERVICE)
@@ -33,8 +31,7 @@ async function authenticateUser(user)
         return await axios.post(url + 'authenticate', 
         {
             email: user.email,
-            password: user.password,
-            role: user.role
+            password: user.password
         })   
     }
     catch(error)
@@ -43,14 +40,29 @@ async function authenticateUser(user)
         else{throw new Error('Internal Server Error')}   
     }
 }
+async function getOTP(email) 
+{
+    const url = await getServiceURL(AUTHENTICATION_SERVICE)
+    if(!url){throw new Error('ER_SERVICE_UNAVAILABLE')}
+
+    try
+    {
+        return await axios.post(url + 'forgot-password', {email})   
+    }
+    catch(error)
+    {
+        if(error.response){return error.response}
+        else{throw new Error('Internal Server Error')}   
+    }
+}
+
 
 module.exports.login = async (req,res) => 
 {   
     const loginSchema = Joi.object
     ({
         email: Joi.string().email().required(),
-        password: Joi.string().required(),
-        role: Joi.string().valid('teacher', 'student').required()
+        password: Joi.string().required()
     })
 
     const { error, value: user } = loginSchema.validate(req.body)
@@ -62,7 +74,7 @@ module.exports.login = async (req,res) =>
         if ( response.status === 200 && response.data.userData ) 
         {
             let user = response.data.userData;
-            const sessionId = await createSession(user)
+            const sessionId = await createSession(user._id)
             const accessToken = createAccessToken(user, sessionId)
             user.accessToken = accessToken
             return res.status(200).json({message: 'Login Successful', user})  
@@ -110,15 +122,58 @@ module.exports.refresh = async (req, res) =>
         res.status(500).json({error: 'ER_INT_SERV', message: 'Failed to refresh token'})
     }
 }
+module.exports.forgotPassword = async (req,res) => 
+{
+    const email = req.body.email
+    if(!email){return res.status(400).json({ error: 'ER_MSG_ARG', message: 'Required: email' })} 
+
+    try{
+        const response = await getOTP(email)
+
+        if ( response.status === 200 && response.data.otp ) 
+            {
+                let otp = response.data.otp
+                await client.setEx(email, 180, otp)
+                console.log(otp)
+                return res.status(200).json({message: 'OTP Sent'})  
+            } 
+            else {return res.status(response.status).json({ error: response.data.error, message: response.data.message })}
+    }
+    catch(err)
+    {
+        res.status(500).json({  error: 'ER_INT_SERV', message: 'Failed to generate OTP' })
+    }
+}
+module.exports.verifyOTP = async (req,res) => 
+    {
+        const OTPSchema = Joi.object
+        ({
+            email: Joi.string().email().required(),
+            otp: Joi.number().required()
+        })
+
+        const { error, value } = OTPSchema.validate(req.body)
+        if (error) {return res.status(400).json({ error: error.name, message: error.message })} 
+    
+        try{
+            const systemOTP = await client.get(value.email)
+            const userOTP = value.otp
+    
+            if ( systemOTP == userOTP ) 
+            {
+                await client.del(value.email)
+                return res.status(200).json({message: 'Verification complete', verified: true})
+            } 
+            else {return res.status(400).json({ error: 'ER_VERF', message: 'Invalid OTP' })}
+        }
+        catch(err)
+        {
+            res.status(500).json({  error: 'ER_INT_SERV', message: 'Failed to verify OTP' })
+        }
+    }
 module.exports.logout = async (req,res) => 
 {
-    const authHeader = req.headers['authorization']
-    const accessToken = authHeader && authHeader.split(' ')[1]
-
-    if(!accessToken){return res.status(401).json({error: true, message: "Unauthenticated"})}
-
-    const decodedToken = jwt.decode(accessToken)
-    const id = decodedToken.id
+    const id = req.body.decodedToken.id
 
     try{
         await client.hDel(SESSION_HASH_KEY, id)
