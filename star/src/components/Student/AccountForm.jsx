a89140d99d52272b95ee09a7aeeeec81ae46d980
Saifullah Khan <saif4rmkhan@gmail.com>
2024-05-15 01:57:23 +0500

    accountpage done

--- star/src/components/Student/AccountForm.jsx
+++ star/src/components/Student/AccountForm.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
+import { MdModeEdit } from "react-icons/md";
 
 const AccountForm = () => {
+  let user = JSON.parse(localStorage.getItem('userDetails'));
   const [formData, setFormData] = useState({
     name: 'John',
     lastName: 'Doe',
@@ -12,6 +14,7 @@ const AccountForm = () => {
   });
 
   const [showPassword, setShowPassword] = useState(false);
+  const [showDialog, setShowDialog] = useState(false); // State for dialog visibility
 
   const handleChange = (e) => {
     setFormData({
@@ -22,6 +25,13 @@ const AccountForm = () => {
 
   const handlePasswordToggle = () => {
     setShowPassword(!showPassword);
+  };
+
+  const handleDialogToggle = () => {
+    setShowDialog(!showDialog);
+  };
+  const handleDialogSave = () => {
+    setShowDialog(!showDialog);
   };
 
   return (
@@ -35,7 +45,6 @@ const AccountForm = () => {
             name="name"
             onChange={handleChange}
             className="pl-2 outline-none focus:outline-none bg-blue-50"
-
           />
         </div>
         <div className='h-16 w-full border bg-blue-50 rounded-md shadow-md flex flex-col'>
@@ -60,6 +69,7 @@ const AccountForm = () => {
             className="pl-2 outline-none focus:outline-none bg-blue-50"
           />
         </div>
+        { user.role === 'student' &&
         <div className='h-16 w-full border bg-blue-50 rounded-md shadow-md flex flex-col'>
           <span className='pl-2 pt-2 text-xs font-light'>Student ID</span>
           <input
@@ -69,42 +79,60 @@ const AccountForm = () => {
             onChange={handleChange}
             className="pl-2 outline-none focus:outline-none bg-blue-50"
           />
-        </div>
+        </div>}
+      </div>
+      <div className='flex gap-2 items-center mb-4'>
+        <button className='bg-DarkBlue text-white rounded-lg shadow-lg px-4 py-2' onClick={handleDialogToggle}>Change Password</button>
       </div>
 
-      <div className='h-16 w-full md:w-8/12 border bg-blue-50 rounded-md shadow-md flex flex-col mb-4'>
-        <span className='pl-2 pt-2 text-xs font-light'>Password</span>
-        <div className="relative">
-          <input
-            type={showPassword ? 'text' : 'password'}
-            value={formData.currentPassword}
-            name="currentPassword"
-            onChange={handleChange}
-            className="pl-2 outline-none focus:outline-none bg-blue-50"
-          />
-          <div
-            className="absolute top-1/2 right-2 transform -translate-y-1/2 cursor-pointer pr-2"
-            onClick={handlePasswordToggle}
-          >
+      {/* Dialog Box */}
+      {showDialog && (
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white rounded-lg p-8 shadow-md">
+            <h2 className="text-2xl font-bold mb-4">Change Password</h2>
+            <div>
+              <div className='flex items-center justify-center rounded-lg bg-blue-50 p-4 mb-4'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              name="Current Password"
+              onChange={handleChange}
+              placeholder="Current Password"
+              className="  outline-none focus:outline-none bg-blue-50"
+            />
+            <div  onClick={handlePasswordToggle}>
+            
             {showPassword ? (
-              <FaRegEyeSlash size={20} color="#2C6491" />
-            ) : (
-              <FaRegEye size={20} color="#2C6491" />
-            )}
+                <FaRegEyeSlash size={20} color="#2C6491" />
+              ) : (
+                <FaRegEye size={20} color="#2C6491" />
+              )}
+            </div>
+            </div>
+            <div className='flex items-center justify-center rounded-lg bg-blue-50 p-4 mb-4'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              value={formData.newPassword}
+              name="newPassword"
+              onChange={handleChange}
+              placeholder="New Password"
+              className=" outline-none focus:outline-none bg-blue-50 "
+            />
+            <div  onClick={handlePasswordToggle}>
+            {showPassword ? (
+                <FaRegEyeSlash size={20} color="#2C6491" />
+              ) : (
+                <FaRegEye size={20} color="#2C6491" />
+              )}
+              </div>
+            </div>
+            </div>
+            <div className='flex gap-2 justify-end'>
+            <button className='bg-gray-400 text-white rounded-lg shadow-lg px-4 py-2' onClick={handleDialogToggle}>Cancel</button>
+            <button className='bg-DarkBlue text-white rounded-lg shadow-lg px-4 py-2 ' onClick={handleDialogSave}>Save</button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='h-16 w-full md:w-8/12 border bg-blue-50 rounded-md shadow-md flex flex-col mb-4'>
-        <span className='pl-2 pt-2 text-xs font-light'>Confirm Password</span>
-        <input
-          type="password"
-          value={formData.newPassword}
-          name="newPassword"
-          onChange={handleChange}
-          className="pl-2 outline-none focus:outline-none bg-blue-50"
-        />
-        
-      </div>
+      )}
     </div>
   );
 };
