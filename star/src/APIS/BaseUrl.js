649ac2c248a865b8e2e5e486e611ba51ea842c99
Saifullah Khan <saif4rmkhan@gmail.com>
2024-05-21 18:26:32 +0500

    landingpage & LMTable

--- star/src/APIS/BaseUrl.js
+++ star/src/APIS/BaseUrl.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseUrl = 'http://localhost:3000/'
+export const baseUrl = 'http://57.151.12.166:3000/'
 
 export const AxiosBase = axios.create({
     baseURL: baseUrl
