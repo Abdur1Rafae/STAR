a5d1ee8fbc1ed48af970a7ecc533937c0b543272
Saifullah Khan <saif4rmkhan@gmail.com>
2024-06-01 01:43:28 +0500

    added showandhide password

--- star/src/pages/AuthenticationPages/Login.jsx
+++ star/src/pages/AuthenticationPages/Login.jsx
@@ -2,68 +2,68 @@ import React, { useState, useEffect } from 'react';
 import logo from '../../components/logo.png';
 import studentimage from './student-img.png';
 import { UserLogin } from '../../APIS/AuthAPI';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
+
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('')
-  const [check, setCheck] = useState(true)
+  const [error, setError] = useState('');
+  const [check, setCheck] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
 
-  useEffect(()=> {
-    localStorage.clear()
-  }, [])
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
 
-  const handleSubmit = async() => {
-    setError('')
-    setCheck(false)
-    if(email == '') {
-      setCheck(true)
-      setError('Email Required')
-      return
+  const handleSubmit = async () => {
+    setError('');
+    setCheck(false);
+    if (email === '') {
+      setCheck(true);
+      setError('Email Required');
+      return;
     }
-    if(password == '') {
-      setCheck(true)
-      setError('Password Required')
-      return
+    if (password === '') {
+      setCheck(true);
+      setError('Password Required');
+      return;
     }
     try {
       const response = await UserLogin({
-         email: email,
-         password: password,
+        email: email,
+        password: password,
       });
-      if (response.status == 200) {
-        localStorage.setItem('token', response.data.user.accessToken)
-        localStorage.setItem('userDetails', JSON.stringify(response.data.user))
-        localStorage.setItem('userEmail' , email )
-        if(response.data.user.role == 'student') {
-         window.location.assign('/student/home')
-        }
-        else {
-         window.location.assign('/teacher/home')
+      if (response.status === 200) {
+        localStorage.setItem('token', response.data.user.accessToken);
+        localStorage.setItem('userDetails', JSON.stringify(response.data.user));
+        localStorage.setItem('userEmail', email);
+        if (response.data.user.role === 'student') {
+          window.location.assign('/student/home');
+        } else {
+          window.location.assign('/teacher/home');
         }
         console.log('Login successful:', response.data);
       }
-    } catch (error) {    
-      if(error.response.status == 404) {
-        setError('User Not Found')
+    } catch (error) {
+      if (error.response.status === 404) {
+        setError('User Not Found');
+      } else if (error.response.status === 400) {
+        setError('Invalid Email');
+      } else if (error.response.status === 401) {
+        setError('Invalid Credentials');
       }
-      else if(error.response.status == 400) {
-        setError('Invalid Email')
-      }
-      else if(error.response.status == 401) {
-        setError('Invalid Credentials')
-      }
-      setCheck(true)
+      setCheck(true);
       console.log('Error:', error);
     }
   };
 
   return (
     <div className='w-full h-screen font-body bg-LightBlue sm:overflow-y-auto'>
-      <div className={`menu-container w-full h-14 bg-DarkBlue flex items-center `}>
+      <div className={`menu-container w-full h-14 bg-DarkBlue flex items-center`}>
         <div className='w-full'>
           <div className="menuleft logo flex justify-start">
-            <img src={logo} className='w-44 h-14 mr-2'></img>
+            <img src={logo} className='w-44 h-14 mr-2' alt="Logo"></img>
           </div>
         </div>
       </div>
@@ -71,42 +71,49 @@ const Login = () => {
         <div className='mt-4 lg:mt-16'>
           <h1 className='font-bold md:text-2xl text-center ml-2'>Welcome back to the journey of learning! <br /> Let's make it a great one.</h1>
           <div className='w-full flex justify-center'>
-            <img src={studentimage} alt=""  className='md:w-[700px] lg:w-[900px]'></img>
+            <img src={studentimage} alt="Student" className='md:w-[700px] lg:w-[900px]'></img>
           </div>
         </div>
         <div className='-mt-16 m-4 lg:m-16 h-fit max-md:py-8 md:p-4 flex flex-col items-center justify-center'>
           <p className='text-xs text-gray-500 flex flex-col items-center mt-8'>Please login into your account</p>
           <p className='text-xs text-red-500 flex flex-col items-center mt-2'>{error}</p>
           <form className=' w-full flex flex-col'>
-               <label className='text-md font-semibold'>Email</label>
-               <input 
-               type="email" 
-               placeholder='Enter your Email' 
-               className='rounded-lg text-xs h-fit w-full my-2 py-4 px-2'
-               value={email}
-               onChange={(e) => setEmail(e.target.value)}
-               />
-            <label className='text-md font-semibold mt-2'>Password</label>
+            <label className='text-md font-semibold'>Email</label>
             <input 
-              type="password" 
-              placeholder='Password' 
+              type="email" 
+              placeholder='Enter your Email' 
               className='rounded-lg text-xs h-fit w-full my-2 py-4 px-2'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
+            <label className='text-md font-semibold mt-2'>Password</label>
+            <div className='relative w-full'>
+              <input 
+                type={showPassword ? "text" : "password"} 
+                placeholder='Password' 
+                className='rounded-lg text-xs h-fit w-full my-2 py-4 px-2 pr-10'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div className='absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5'>
+                <button type="button" onClick={() => setShowPassword(!showPassword)}>
+                  {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+                </button>
+              </div>
+            </div>
             <div className='flex items-center justify-between'>
               <div className='flex items-center gap-2'>
                 <input type="checkbox" className='h-fit'/>
                 <label className='text-xs'>Remember me</label>
               </div>
               <div>
-                <button type='button' onClick={()=>{window.location.assign('/forgot-password')}}  className='text-xs text-[#2D79F3]'>Forgot Password?</button>
+                <button type='button' onClick={() => {window.location.assign('/forgot-password')}} className='text-xs text-[#2D79F3]'>Forgot Password?</button>
               </div>
             </div>
-            <button type="button" onClick={check ? handleSubmit : ()=> {}} className={`w-52 mt-8 mb-4 rounded-lg ${check ? 'bg-DarkBlue': 'bg-slate-500' } text-white text-sm py-4 self-center`}>Login</button>
+            <button type="button" onClick={check ? handleSubmit : () => {}} className={`w-52 mt-8 mb-4 rounded-lg ${check ? 'bg-DarkBlue': 'bg-slate-500' } text-white text-sm py-4 self-center`}>Login</button>
             <div className='flex items-center justify-center gap-2 self-center'>
               <span className='text-xs'>Don't have an Account? </span>
-              <button type='button' onClick={()=>{window.location.assign('/signup')}} className='text-xs text-[#2D79F3]'>Sign Up</button>
+              <button type='button' onClick={() => {window.location.assign('/signup')}} className='text-xs text-[#2D79F3]'>Sign Up</button>
             </div>
           </form>
         </div>
--- star/src/pages/AuthenticationPages/Signup.jsx
+++ star/src/pages/AuthenticationPages/Signup.jsx
@@ -1,73 +1,74 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 import logo from '../../components/logo.png';
 import { UserSignup } from '../../APIS/AuthAPI';
 import cover2 from './cover2.png';
 
 const Signup = () => {
   const [isTeacherMode, setIsTeacherMode] = useState(true);
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [erp, setErp] = useState(0)
-  const [check, setCheck] = useState(true)
-  const [error, setError] = useState('')
+  const [erp, setErp] = useState('');
+  const [check, setCheck] = useState(true);
+  const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit = async() => {
-    setCheck(false)
-    setError('')
-    if(email == '') {
-      setCheck(true)
-      setError('Email Required')
-      return
+  const handleSubmit = async () => {
+    setCheck(false);
+    setError('');
+    if (email === '') {
+      setCheck(true);
+      setError('Email Required');
+      return;
     }
-    if(password == '') {
-      setCheck(true)
-      setError('Password Required')
-      return
+    if (password === '') {
+      setCheck(true);
+      setError('Password Required');
+      return;
     }
-    if(firstName == '' || lastName == ''){
-      setCheck(true)
-      setError('Name Required')
-      return
+    if (firstName === '' || lastName === '') {
+      setCheck(true);
+      setError('Name Required');
+      return;
     }
-    if(erp == '' && !isTeacherMode) {
-      setCheck(true)
-      setError('ERP Required')
-      return
+    if (erp === '' && !isTeacherMode) {
+      setCheck(true);
+      setError('ERP Required');
+      return;
     }
     try {
-      let obj = {}
-      if(isTeacherMode) {
+      let obj = {};
+      if (isTeacherMode) {
         obj = {
           name: firstName + ' ' + lastName,
           email: email,
           password: password,
           role: 'teacher'
-        }
-      }
-      else {
+        };
+      } else {
         obj = {
           name: firstName + ' ' + lastName,
           email: email,
           erp: erp,
           password: password,
           role: 'student'
-        }
+        };
       }
-      const response = await UserSignup({obj: obj});
-      if (response.status == 200) {
-        setCheck(true)
-        window.location.assign('/login')
-        console.log('Login successful:', response.data);
+      const response = await UserSignup({ obj: obj });
+      if (response.status === 200) {
+        setCheck(true);
+        window.location.assign('/login');
+        console.log('Signup successful:', response.data);
       } else {
-        console.log('Login failed:', response.statusText);
+        console.log('Signup failed:', response.statusText);
       }
-    } catch (error) { 
-      if(error.response.status == 400) {
-        setError('User Already Exists')
+    } catch (error) {
+      if (error.response.status === 400) {
+        setError('User Already Exists');
       }
-      setCheck(true)   
+      setCheck(true);
       console.log('Error:', error);
     }
   };
@@ -77,71 +78,76 @@ const Signup = () => {
       <div className={`menu-container w-full h-14 bg-DarkBlue flex items-center `}>
         <div className='w-full'>
           <div className="menuleft logo flex justify-start">
-            <img src={logo} className='w-44 h-14 mr-2'></img>
+            <img src={logo} className='w-44 h-14 mr-2' alt="Logo"></img>
           </div>
         </div>
       </div>
       <div className='md:flex gap-4 p-8 h-fit overflow-x-hidden'>
-            <div className='flex flex-col justify-end md:min-w-96'>
-                <h1 className='font-bold text-center text-2xl md:text-left leading-relaxed'>Are you ready to take control of your learning journey?</h1>
-                <h2 className='text-DarkBlue text-center text-3xl md:text-left font-semibold mt-8'>Register Now!</h2>
-                <div className=' flex  flex-col  items-center  mt-8'>
-                <img src={cover2} alt="" className='md:w-64  '/>
-                </div>
-            </div>  
-            <div className='flex-auto border border-black p-4 rounded-lg max-md:mt-8'>
-                    <div className='flex flex-col items-center'>
-                    <div className="w-max bg-white border-2 text-xs border-black rounded-full flex overflow-hidden">
-                    <button
-                        type="button"
-                        className={`px-4 py-2 flex items-center tracking-wider font-semibold outline-none transition-all duration-300 ${
-                        isTeacherMode ? 'bg-DarkBlue text-white' : 'bg-gray-50 text-black'
-                        }`}
-                        onClick={() => setIsTeacherMode(true)}
-                    >
-                        Teacher
-                    </button>
-                    <button
-                        type="button"
-                        className={`px-4 py-2 flex items-center tracking-wider font-semibold outline-none transition-all duration-300 ${
-                        isTeacherMode ? 'bg-gray-50 text-black' : 'bg-DarkBlue text-white'
-                        }`}
-                        onClick={() => setIsTeacherMode(false)}
-                    >
-                        Student
-                    </button>
-                    </div>
-                </div>
-                <form className='py-4 text-sm flex-grow min-w-72' action="">
-                    <div className='w-full flex items-center justify-center'>
-                    <p className='text-red-600 mx-auto'>{error}</p>
-                    </div>
-                    <div className='w-full md:flex gap-4 mt-4'>
-                      <input onChange={(e) => setFirstName(e.target.value)} value={firstName} placeholder= "First Name" type="text" className='max-md:mt-4 w-full border border-gray-200 rounded-lg p-2'/>
-                      <input onChange={(e) => setLastName(e.target.value)} value={lastName} placeholder= "Last Name" type="text" className='max-md:mt-4 w-full border border-gray-200  rounded-lg p-2'/>
-                    </div>
-                    <div className=''>
-                        <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder= "Email" type="email" className='mt-4 w-full border  border-gray-200 rounded-lg p-2'/>
-                    </div>
-                    {
-                      !isTeacherMode ?
-                      <div>
-                        <input onChange={(e) => setErp(e.target.value)} value={erp} placeholder= "ERP" type="text" className=' mt-4 w-full border border-gray-200 rounded-lg p-2'/>
-                    </div>
-                    :''
-                    }
-                    <div>
-                        <input onChange={(e) => setPassword(e.target.value)} value={password} placeholder= "Password" type="password" className='mt-4 w-full border border-gray-200 rounded-lg p-2'/>
-                    </div>
-                    <div className='flex flex-col items-center' >
-                    <button onClick={check ? handleSubmit : ()=> {}} type="button"  className={`w-52 mt-8 mb-4 rounded-lg ${check ? 'bg-DarkBlue': 'bg-slate-500' } text-white text-sm py-4 self-center flex flex-col items-center`}>Signup</button>
-                    </div>
-                </form>
-                <div className='flex items-center justify-center gap-2 self-center'>
-                    <span className='text-xs'>Already have an Account </span>
-                    <button onClick={()=>{window.location.assign('/login')}} className='text-xs text-[#2D79F3]'>Login</button>
-                </div>
+        <div className='flex flex-col justify-end md:min-w-96'>
+          <h1 className='font-bold text-center text-2xl md:text-left leading-relaxed'>Are you ready to take control of your learning journey?</h1>
+          <h2 className='text-DarkBlue text-center text-3xl md:text-left font-semibold mt-8'>Register Now!</h2>
+          <div className='flex flex-col items-center mt-8'>
+            <img src={cover2} alt="" className='md:w-64' />
+          </div>
+        </div>
+        <div className='flex-auto border border-black p-4 rounded-lg max-md:mt-8'>
+          <div className='flex flex-col items-center'>
+            <div className="w-max bg-white border-2 text-xs border-black rounded-full flex overflow-hidden">
+              <button
+                type="button"
+                className={`px-4 py-2 flex items-center tracking-wider font-semibold outline-none transition-all duration-300 ${isTeacherMode ? 'bg-DarkBlue text-white' : 'bg-gray-50 text-black'}`}
+                onClick={() => setIsTeacherMode(true)}
+              >
+                Teacher
+              </button>
+              <button
+                type="button"
+                className={`px-4 py-2 flex items-center tracking-wider font-semibold outline-none transition-all duration-300 ${isTeacherMode ? 'bg-gray-50 text-black' : 'bg-DarkBlue text-white'}`}
+                onClick={() => setIsTeacherMode(false)}
+              >
+                Student
+              </button>
             </div>
+          </div>
+          <form className='py-4 text-sm flex-grow min-w-72'>
+            <div className='w-full flex items-center justify-center'>
+              <p className='text-red-600 mx-auto'>{error}</p>
+            </div>
+            <div className='w-full md:flex gap-4 mt-4'>
+              <input onChange={(e) => setFirstName(e.target.value)} value={firstName} placeholder="First Name" type="text" className='max-md:mt-4 w-full border border-gray-200 rounded-lg p-2' />
+              <input onChange={(e) => setLastName(e.target.value)} value={lastName} placeholder="Last Name" type="text" className='max-md:mt-4 w-full border border-gray-200 rounded-lg p-2' />
+            </div>
+            <div>
+              <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder="Email" type="email" className='mt-4 w-full border border-gray-200 rounded-lg p-2' />
+            </div>
+            {!isTeacherMode &&
+              <div>
+                <input onChange={(e) => setErp(e.target.value)} value={erp} placeholder="ERP" type="text" className='mt-4 w-full border border-gray-200 rounded-lg p-2' />
+              </div>
+            }
+            <div className='relative w-full'>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                placeholder="Password"
+                type={showPassword ? "text" : "password"}
+                className='mt-4 w-full border border-gray-200 rounded-lg p-2 pr-10'
+              />
+              <div className='absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5'>
+                <button type="button" onClick={() => setShowPassword(!showPassword)}>
+                  {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+                </button>
+              </div>
+            </div>
+            <div className='flex flex-col items-center'>
+              <button onClick={check ? handleSubmit : () => { }} type="button" className={`w-52 mt-8 mb-4 rounded-lg ${check ? 'bg-DarkBlue' : 'bg-slate-500'} text-white text-sm py-4 self-center flex flex-col items-center`}>Signup</button>
+            </div>
+          </form>
+          <div className='flex items-center justify-center gap-2 self-center'>
+            <span className='text-xs'>Already have an Account</span>
+            <button onClick={() => { window.location.assign('/login') }} className='text-xs text-[#2D79F3]'>Login</button>
+          </div>
+        </div>
       </div>
     </div>
   );
